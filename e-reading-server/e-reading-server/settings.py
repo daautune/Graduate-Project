f90b5743ff1027a7382938111ab31871be671efb
Marsch Huynh <marsch.huynh@gmail.com>
2019-09-27 17:37:02 +0700

    chore: minor update

--- e-reading-server/e-reading-server/settings.py
+++ e-reading-server/e-reading-server/settings.py
@@ -88,18 +88,18 @@ WSGI_APPLICATION = 'e-reading-server.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ereadingdb',
-        'USER': 'ereadingserver',
-        'PASSWORD': 'ereadingserver',
-        'HOST': '10.0.0.4',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'ereadingdb',
+    #     'USER': 'ereadingserver',
+    #     'PASSWORD': 'ereadingserver',
+    #     'HOST': '10.0.0.4',
+    #     'PORT': 5432,
+    # },
 }
 
 CACHES = {
